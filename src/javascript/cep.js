ffeeeb1ad72e6fd44b693321695e94a7dd41e1ae
Vitor Vilarim <vitorbvilarim@gmail.com>
2024-02-19 13:01:24 -0300

    correção function cep

--- src/javascript/cep.js
+++ src/javascript/cep.js
@@ -1,29 +1,22 @@
-function buscarCep() {
-    let cep = document.getElementById("cep-consultar").value
 
-    if (cep !== '') {
-        let url = `https://brasilapi.com.br/api/cep/v1/${cep}`
 
-        let req = new XMLHttpRequest()
-        let.open("GET", url)
-        req.send()
 
-        req.onload = () => {
-            if (req.status === 200) {
-                let endereco = JSON.parse(req.response)
-                console.log(endereco)
-                document.getElementById('rua').value = endereco.street
-                document.getElementById('estado').value = endereco.state
-                document.getElementById('cidade').value = endereco.city
-            }
-        }
+let cepHtml = document.getElementById("cep-consultar");
+cepHtml.addEventListener('blur', async () => {
+
+    let cepConsultar = document.getElementById("cep-consultar").value
+
+
+    if (cepConsultar !== '') {
+        let urlCep = `https://brasilapi.com.br/api/cep/v1/${cepConsultar}`
+
+        const dadosEndereco = await axios.get(urlCep)
+
+        const { street, state, city } = dadosEndereco.data
+
+        document.getElementById('rua-consultar').value = street
+        document.getElementById('estado-consultar').value = state
+        document.getElementById('cidade-consultar').value = city
+        console.log(dadosEndereco.data)
     }
-
-}
-
-buscarCep('58402060')
-
-window.onload = () => {
-    let cepHtml = document.getElementById("cep");
-    cepHtml.addEventListener("cnahge", buscarCep())
-}
+})
